2601b436cfb1db8ba90fa4944e360c23f643a846
Christine Doherty <christineadoherty@gmail.com>
2019-12-11 20:42:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
 	    {reading_railroad: {},
 	     pennsylvania_railroad: {},
 	     b_and_o_railroad: {},
-	     name4: 4
+	     shortline_railroad: {}
 	    },
 	    rent_in_dollars: 
 	    {one_piece_owned: 25, 
